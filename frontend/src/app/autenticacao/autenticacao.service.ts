a5b2459930f264716e49e488187af176875088d4
dcmunhoz <dc.munhoz@hotmail.com>
2022-02-27 00:26:49 -0300

    Implementing authentication

--- frontend/src/app/autenticacao/autenticacao.service.ts
+++ frontend/src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,26 @@ export class AutenticacaoService {
 
   private _api: HttpClient;
 
-  constructor( httpClient: HttpClient ) { 
+  constructor( 
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+   ) { 
     
     this._api = httpClient;
 
   }
 
-  autenticar(usuario: string, senha: string): Observable<any> {
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
     return this._api.post("http://localhost:3000/user/login", {
       userName: usuario,
       password: senha
-    })
+    }, {
+      observe: 'response'
+    }).pipe(
+      tap((response) => {
+        const authToken = response.headers.get('x-access-token') ?? '';
+        this.usuarioService.salvaToken(authToken);
+      })
+    )
   }
 }
