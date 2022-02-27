8509ad4efd63fe071bc3c39c46848ddc6748e736
dcmunhoz <dc.munhoz@hotmail.com>
2022-02-26 23:31:39 -0300

    Creating new user

---
+++ frontend/src/app/home/novo-usuario/minusculo.validator.ts
@@ -0,0 +1,7 @@
+import { AbstractControl } from "@angular/forms";
+
+export function minusculoValidator(control: AbstractControl){
+    const valor = control.value as string;
+    
+    return valor !== valor.toLocaleLowerCase() ? { minusculo: true } : null
+}
