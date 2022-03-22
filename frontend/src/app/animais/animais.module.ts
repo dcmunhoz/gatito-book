d154547f2139524bc47e26d0a1d34b41cf78a007
dcmunhoz <dc.munhoz@hotmail.com>
2022-03-22 20:52:26 -0300

    resolver

--- frontend/src/app/animais/animais.module.ts
+++ frontend/src/app/animais/animais.module.ts
@@ -10,6 +10,7 @@ import { AnimalDetalheComponent } from './animal-detalhe/animal-detalhe.componen
 import { ComentariosComponent } from './animal-detalhe/comentarios/comentarios.component';
 import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -24,8 +25,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     AnimaisRoutingModule,
     CartaoModule,
-    MensagemModule,
-    ReactiveFormsModule
+    SharedModule
   ]
 })
 export class AnimaisModule { }
--- frontend/src/app/animais/lista-animais/lista-animais.component.ts
+++ frontend/src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { Animais } from '../animais';
@@ -11,9 +12,9 @@ import { AnimaisService } from '../animais.service';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  animais$ !: Observable<Animais>;
+  animais!: Animais;
 
-  constructor(private usuarioService: UsuarioService, private animaisService: AnimaisService) { }
+  constructor(private acRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     // this.usuarioService.retornaUsuario().subscribe(usuario => {
@@ -24,12 +25,16 @@ export class ListaAnimaisComponent implements OnInit {
     //   });
     // });
 
-    this.animais$ = this.usuarioService.retornaUsuario().pipe(
-      switchMap((usuario => {
-        const userName = usuario.name ?? '';
-        return this.animaisService.listaDoUsuario(userName);
-      }))
-    );
+    // this.animais$ = this.usuarioService.retornaUsuario().pipe(
+    //   switchMap((usuario => {
+    //     const userName = usuario.name ?? '';
+    //     return this.animaisService.listaDoUsuario(userName);
+    //   }))
+    // );
+
+    this.acRoute.params.subscribe(params => {
+      this.animais = this.acRoute.snapshot.data['animais'];
+    });
   }
 
 }
